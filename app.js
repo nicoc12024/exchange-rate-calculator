e85984a44408283ee32f87fd01fd30e9b0a0f606
Nicolas <56944680+nicoc12024@users.noreply.github.com>
2022-04-27 11:20:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,19 +5,16 @@ const amountEl_two = document.getElementById("amount-two");
 const swapBtn = document.getElementById("swap");
 const rateEl = document.getElementById("rate");
 
+
 // Fetch exchange rates and update the DOM
 function calculate() {
-  const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
   const apiKey = "043c95279cb1f71c37b01b88";
 
-  fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency_one}`)
+  fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currencyEl_one.value}`)
     .then((res) => res.json())
     .then((data) => {
-      //console.log(data);
-      const rate = data.conversion_rates[currency_two];
-
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      const rate = data.conversion_rates[currencyEl_two.value];
+      rateEl.innerText = `1 ${currencyEl_one.value} = ${rate} ${currencyEl_two.value}`;
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
